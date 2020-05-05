cd357853f53e90973cae2ae80c96db701a95470e
Fabian Lopez <Fabian.X.Lopez.-ND@disney.com>
2020-05-04 22:48:42 -0500

    updated

--- TallerFinal/example.tf
+++ TallerFinal/example.tf
@@ -72,17 +72,3 @@ resource "aws_elb" "example" {
     instance_protocol = "http"
   }
 }
-## Creating AutoScaling Group
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["${data.aws_availability_zones.all.names[0]}"]
-  min_size = 2
-  max_size = 4
-  load_balancers = ["${aws_elb.example.name}"]
-  health_check_type = "ELB"
-  tag {
-    key = "Name"
-    value = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
